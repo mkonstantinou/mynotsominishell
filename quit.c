43212dedaf8371318828864e10defe9dded1e607
mkonstantinou <mkonstan@stevens.edu>
2014-05-10 23:17:25 -0400

    fixed segmentation fault on exit

--- quit.c
+++ quit.c
@@ -19,36 +19,10 @@ void quit(int n)
     }
 
     close(fd);
-    free(filename);
-
-    
-    getout();
-
     //exit
     my_str("\nThanks for playing\n");
     
     ioctl(0, TCSETA, &(gl_env.line_backup));
 	dup2(gl_env.stdio_backup, 1);
 	exit(0);
-}
-
-void getout()
-{
-    free(gl_env.history);
-    free(gl_env.left);
-    free(gl_env.right);
-    free(gl_env.up);
-    free(gl_env.down);
-    free(gl_env.esc);
-    free(gl_env.backspace);
-    free(gl_env.underline);
-    free(gl_env.under_end);
-    free(gl_env.standout);
-    free(gl_env.stand_end);
-    free(gl_env.move);
-    free(gl_env.clear);
-    free(gl_env.cursoroff);
-    free(gl_env.cursoron);
-    free(gl_env.strbuff);
-    free(gl_env.clipboard);
 }
